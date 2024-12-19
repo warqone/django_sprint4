8f693cda0f351a56eb5a16aa8f14ec101ddb3217
Vadim Karimov <warqone12@yandex.ru>
2024-12-19 23:47:30 +0500

    small fix

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,6 +1,5 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
-from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, render, redirect
 
 from blog.forms import PostForm, EditProfileForm, CommentForm
